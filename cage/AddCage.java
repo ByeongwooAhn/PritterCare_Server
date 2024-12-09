41f378598882fb7542f116cbac863d6a4d5066e5
ByeongwooAhn <abyungwoo@gmail.com>
2024-12-09 14:49:37 +0900

    feat: 서버에서 케이지의 시리얼 넘버 유무를 체크하여 유저네임을 삽입하는 코드 추가.

--- cage/AddCage.java
+++ cage/AddCage.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +28,38 @@ public class AddCage {
 	 @Value("${spring.datasource.password}")
 	 private String dbPassword;
 	 
-	 @PostMapping("/add")
+	 @PatchMapping("/serial")
+	 public ResponseEntity<String> serialCheck(@RequestHeader("Authorization") String requestHeader, @RequestBody Cage requestBody) {
+		 if(requestHeader != null) {
+			 String token = requestHeader;
+			 String username = JwtUtil.extractUsername(token);
+			 
+			 String sql = "UPDATE cages SET username = ? WHERE cage_serial_number = ?;";
+			 
+			 try(Connection con = DriverManager.getConnection(url, dbUsername, dbPassword);
+					 PreparedStatement pstmt = con.prepareStatement(sql)) {
+				 
+				 String cage_serial_number = requestBody.getCage_serial_number();
+				 
+				 pstmt.setString(1, username);
+				 pstmt.setString(2, cage_serial_number);
+				 
+				 int rowsAffected = pstmt.executeUpdate();
+				if(rowsAffected > 0) {
+					return ResponseEntity.ok("Add Username Successful for Cage!");
+				} else {
+					return ResponseEntity.status(404).body("Not Found Cage Serial Number."); // 업데이트 할 데이터 없음
+				}
+			 } catch(SQLException e) {
+				 e.printStackTrace();
+				 return ResponseEntity.status(500).body("Error during database query execution: " + e.getMessage()); // SQL 오류
+			 }
+		} else {
+			return ResponseEntity.status(401).body("Authorization header is missing or invalid");  // 인증이 만료되거나 유효하지 않아 실패함
+		}
+	 }
+	 
+	 @PatchMapping("/add")
 	 public ResponseEntity<String> addCage(@RequestHeader ("Authorization") String requestHeader, @RequestBody Cage requestBody) {
 		 
 		 // 헤더에 토큰값이 있으면
@@ -37,7 +68,7 @@ public class AddCage {
 			 String username = JwtUtil.extractUsername(token); // 토큰값에서 username 추출
 			 
 			 // MariaDB에 데이터 삽입
-			 String sql = "INSERT INTO cages VALUES(?, ?, ?, ?, ?, ?, ?, ?);";
+			 String sql = "UPDATE cages SET cage_name = ?, animal_type = ?, env_temperature = ?, env_humidity = ?, env_lighting = ?, env_water_level = ? WHERE cage_serial_number = ? AND username = ?;";
 			 
 			 try(Connection con = DriverManager.getConnection(url, dbUsername, dbPassword);
 					 PreparedStatement pstmt = con.prepareStatement(sql)) {
@@ -52,21 +83,21 @@ public class AddCage {
 				 String env_water_level = requestBody.getEnv_water_level();
 				 
 				 // 전달 받은 데이터 바인딩
-				 pstmt.setString(1, cage_serial_number);
-				 pstmt.setString(2, username);
-				 pstmt.setString(3, cage_name);
-				 pstmt.setString(4, animal_type);
-				 pstmt.setString(5, env_temperature);
-				 pstmt.setString(6, env_humidity);
-				 pstmt.setString(7, env_lighting);
-				 pstmt.setString(8, env_water_level);
+				 pstmt.setString(1, cage_name);
+				 pstmt.setString(2, animal_type);
+				 pstmt.setString(3, env_temperature);
+				 pstmt.setString(4, env_humidity);
+				 pstmt.setString(5, env_lighting);
+				 pstmt.setString(6, env_water_level);
+				 pstmt.setString(7, cage_serial_number);
+				 pstmt.setString(8, username);
 				 
 				 // INSERT 쿼리 실행
 				 int rowsAffected = pstmt.executeUpdate();
 				 if(rowsAffected > 0) {
 					 return ResponseEntity.ok("Add Cage Successful!");
 				 } else {
-					 return ResponseEntity.status(500).body("Failed to Insert Data.");
+					 return ResponseEntity.status(404).body("Not Found Cage Serial Number or Username.");
 				 }
 				 
 			 } catch(SQLException e) {
@@ -74,7 +105,7 @@ public class AddCage {
 				 return ResponseEntity.status(500).body("Error during database query execution: " + e.getMessage());
 			 }
 		 } else {
-			 return ResponseEntity.status(400).body("Authorization header is missing or invalid");
+			 return ResponseEntity.status(401).body("Authorization header is missing or invalid");
 		 }
 	 }
 }
